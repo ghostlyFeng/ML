6155aeb56fef0b54e3e99b2ed22171cf417728ea
ghostlyfeng <ghostlyfeng@outlook.com>
2018-07-23 22:22:53 +0800

    knn

---
+++ knn/knn.py
@@ -0,0 +1,58 @@
+# coding:utf-8
+
+from numpy import *
+from matplotlib import pyplot as plt
+import operator
+
+
+##给出训练数据以及对应的类别
+def createDataSet():
+    group = array([[1.0, 2.0], [1.2, 0.1],[1,0.5], [0.1, 1.4], [0.3, 3.5]])
+    labels = ['A', 'A', 'B','B', 'B']
+    x = group[:,0]
+    y = group[:,1]
+    plt.scatter(x,y,s=25,marker='o')
+    plt.show()
+    return group, labels
+
+
+###通过KNN进行分类
+"""
+input 待输入的测试样本
+dataSet样本集合
+label 样本的标记
+k 是knn的k个临近点
+"""
+def classify(input, dataSet, label, k):
+    dataSize = dataSet.shape[0]
+    ####计算欧式距离
+    diff = tile(input, (dataSize, 1)) - dataSet
+    sqdiff = diff ** 2
+    squareDist = sum(sqdiff, axis=1)  ###行向量分别相加，从而得到新的一个行向量
+    dist = squareDist ** 0.5
+
+    ##对距离进行排序
+    sortedDistIndex = argsort(dist)  ##argsort()根据元素的值从小到大对元素进行排序，返回下标
+    #['B','A','A','B']
+    #统计每个分类的个数
+    classCount = {}
+    for i in range(k):
+        #获取到k个点所对应的分类 'A', 'B'
+        voteLabel = label[sortedDistIndex[i]]
+        ###对选取的K个样本所属的类别个数进行统计
+        classCount[voteLabel] = classCount.get(voteLabel, 0) + 1
+
+    print(classCount)
+    ###选取出现的类别次数最多的类别
+    maxCount = 0
+    for key, value in classCount.items():
+        if value > maxCount:
+            maxCount = value
+            classes = key
+
+    return classes
+
+if __name__ =='__main__':
+    t = [10,1]
+    ds,label = createDataSet()
+    print(classify(t,ds,label,4))
